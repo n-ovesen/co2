ce935b17953c44d783aa5ee55e4e0e53b29dbc23
n-ovesen <me@nikolaiovesen.com>
2022-02-22 23:11:46 +0100

    Actually not shitty code and whatnot!

---
+++ README.md
@@ -0,0 +1,34 @@
+# AQM - Air Quality Monitor
+
+## What/why?
+
+Basically, this is a little thing I whacked together over a weekend, to win a discussion at the office about the HVAC not being adjusted properly.
+
+So this little gadget spits out CO2 and TVOC on a little OLED screen, together with arrows showing the current trend compared to a running average over 10 minutes (5sec polling interval * 120 samples), together with a smiley that shows if it is over, near or well below treshold values I quickly snatched from google(this was to win an argument, not conduct science after all).
+
+I have also soldered on a SD-card breakout, but I'm doing something stupid with that, so I'll probably(*read: never*) update the repo with that functionality later.
+
+This is the whole platformio project, so should be a breeze for you to upload it with that, I'm sure you can wrestle a bit with the arduino IDE to get it done as well!
+
+## Bill of materials
+
+- Adafruit Feather M0
+  (you can do wifi functionality and stuff with that too, but I have no wifi to use at the office anyways)
+- Adafruit FeatherWing Doubler
+- Adafruit FeatherWing OLED - 128x32 OLED
+- 3.7V LiPo Cell
+- IAQ Core C
+
+## Assembly
+
+1. Solder on headers to feather-bits
+2. Solder leads to pads on IAQ
+3. Solder leads to SDA, SCL, 3v3 and GND on the feather doubler. Disregard pullups, as there are some on the OLED, on the same i2c bus (*and the not science part*)
+4. Whack together in the most sensible way you can think of.
+
+## Usage:
+
+1. Power up
+2. Wait for sensor to do it's thing
+3. Read data on screen, or use whatever serial thing to log to computer.
+4. Win argument and get the HVAC tuned up.
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,3 @@
-// iAQ_Core_I2C.ino
-//
-// shows how to use the iAQ-CoreMI library with the sensor connected via I2C. 
-//
-// Copyright (c) 2019 Gregor Christandl
-//
-// connect the iAQ-Core to the Arduino like this:
-//
-// Arduino - iAQ-Core
-// 3.3V ---- VCC
-// GND ----- GND
-// SDA ----- SDA
-// SCL ----- SCL
-// other pins should be left unconnected.
-
 #include <Arduino.h>
 #include <Wire.h>
 #include "iAQCoreTwoWire.h"
@@ -20,100 +5,184 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_FeatherOLED.h>
 #include <SPI.h>
+#include <RunningAverage.h>
 
 
 //iAQ-Core I2C address is fixed, so only one sensor is allowed per I2C bus. 
 iAQCoreTwoWire iaq(&Wire);
 
 //OLED Display object
-Adafruit_FeatherOLED oled = Adafruit_FeatherOLED();
-//Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
-
+//Adafruit_FeatherOLED oled = Adafruit_FeatherOLED();
+Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
 
 String spinner[8] = {"-", "\\", "|", "/", "-", "\\", "|", "/"};
 
-int i = 1;
-int invert = false;
+RunningAverage Co2Avg(120);
+RunningAverage VocAvg(120);
 
-void setup()
-{
-	// put your setup code here, to run once:
+int i = 1;
+
+void setup(){
+
+    if(!oled.begin(SSD1306_SWITCHCAPVCC, 0x3c)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+	
 	Serial.begin(9600);
 
-  //Initiate featherwing display
-  //display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  oled.init();
-  
-
-  //clear display buffer
+  //clear display buffer and set up
   oled.clearDisplay();
-  oled.display();
   oled.setTextColor(SSD1306_WHITE);
+  oled.display();  
   oled.setRotation(2);
+
+  //Print splash screen
   oled.setTextSize(2);
   oled.setCursor(0,0);
   oled.println("AQM");
   oled.setTextSize(1);
   oled.println("\nAir Quality Monitor");
   oled.display();
+  
+  //just a pause, to display the unneccessary splash screen
+  delay(2000);
 
-	//wait for serial connection to open (only necessary on some boards)
-  //while (!Serial);
-
-  #if defined(ESP32)
-	  Wire.begin(SDA, SCL);
-  #elif defined(ESP8266)
-	  Wire.begin(D2, D3);
-  #else
-	  Wire.begin();
-  #endif 
+  Wire.begin(); 
 
 	//iAQ-Core can operate at a maximum of 100kHz clock speed 
 	Wire.setClock(100000L);
 
-  #if defined(ESP8266)
-	//iAQ-Core uses clock stretching, but the default clock stretching limit must be increased for correct operation. 
-	//This is only needed on ESP8266.
-	  Wire.setClockStretchLimit(1000);
-  #endif /* defined(ESP8266) */
+	if (!iaq.begin()){
 
-	if (!iaq.begin())
-	{
 		Serial.println("begin() failed. check the connection to your iAQ-Core.");
+    oled.clearDisplay();
+    oled.setTextSize(2);
+    oled.println("HE DED :(");
 		while (1);
 	}
 }
 
-void loop()
-{
+//Returns the trend of CO2 based on running average over the last 120 samples(10 minutes with 5sec loop)
+
+uint8_t Co2Trend(int c){
+
+  uint8_t s = 0x00;
+  int avg = int(Co2Avg.getAverage());
+
+  if(c > avg){
+
+    s = 0x18;
+  }
+
+  else if(c < avg){
+
+    s = 0x19;
+  }
+
+  else{
+
+    s = 0x1a;
+  }
+
+  return s;
+}
+
+//Returns the trend of TVOC based on running average over the last 120 samples(10 minutes with 5sec loop)
+
+uint8_t VocTrend(int v){
+
+  uint8_t s = 0x00;
+  int avg = int(VocAvg.getAverage());
+
+  if(v > avg){
+
+    s = 0x18;
+  }
+
+  else if(v < avg){
+
+    s = 0x19;
+  }
+
+  else{
+
+    s = 0x1a;
+  }
+
+  return s;
+}
+
+//Returns silly smileys based on whether the CO2 measurment is over treshold values found here: https://www.fhi.no/ml/miljo/inneklima/artikler-inneklima-og-helseplager/karbondioksid-co2-og-inneklima/
+
+String co2Treshold(int c){
+
+  String s = "";
+
+  int co2High = 1000;
+  int co2Med  = 700;
+
+  if(c > co2High){
+
+    s = ":(";
+  }
+
+  else if (c > co2Med){
+
+    s = ":/";
+  }
+
+  else {
+
+    s = ":)";
+  }
+
+  return s;  
+}
+
+//Returns silly smileys based on whether the TVOC measurment is over treshold values found here: https://www.worldgbc.org/sites/default/files/bp-resource/BPFP-IEQ-Guidance-Note.pdf
+
+String vocTreshold(int v){
+
+  String s = "";
+
+  int vocHigh = 500;
+  int vocMed  = 250;
+  
+  if(v > vocHigh){
+
+    s = ":(";
+  }
+
+  else if (v > vocMed){
+
+    s = ":/";
+  }
+
+  else {
+
+    s = ":)";
+  }
+
+  return s;  
+}
+
+void loop(){
+
 	delay(5000);
 
   oled.clearDisplay();
   oled.setCursor(0,0);
-    // get the current voltage of the battery
-
-
-	//read the sensor to determine if it holds a valid value. 
-	//hasValue MUST be called to update sensor values. 
-
-  //Serial.println(i);
-  
-
-
-
-	//Serial.print("sensor has valid value: "); Serial.println(iaq.hasValue() ? "true" : "false");
   int v = iaq.hasValue();
-  
 
+	if (iaq.isRunin()){
 
-	if (iaq.isRunin())
-  {
 		Serial.println("sensor is runin state");
     oled.setTextSize(2);
     oled.println("WARMING UP  SENSOR");
     oled.display();
   }
-
 
 	else if (iaq.isBusy())
 		Serial.println("sensor is busy");
@@ -121,29 +190,41 @@ void loop()
 	else if (iaq.isValid())
 	{
 
-    Serial.print(v); Serial.print(" "); Serial.print(iaq.getCO2()); Serial.print(" "); Serial.print(iaq.getTVOC()); Serial.println("");
+    int co2 = iaq.getCO2();
+    int voc = iaq.getTVOC();
+
+    Co2Avg.addValue(co2);
+    VocAvg.addValue(voc);
+
     oled.setTextSize(1);
-    String co2Val = "co2: " + String(iaq.getCO2()) + " ppm";
-    String vocVal = "voc: " + String(iaq.getTVOC()) + " ppb";
-    oled.println(co2Val);
-    oled.println(vocVal);
-    String sp = "\n                    " + spinner[i];
+    
+    oled.write(Co2Trend(co2));
+    oled.println(" co2: " + String(co2) + " ppm " + co2Treshold(co2));
+
+    oled.write(VocTrend(voc));
+    oled.println(" voc: " + String(voc) + " ppb " + vocTreshold(voc));
+
+    Serial.println(String(co2) + " " + String(Co2Avg.getAverage()) + " " + String(voc) + " " + String(VocAvg.getAverage()));
+
+    String sp = "\n                    " + spinner[i];  //displays a spinner in the lower right corner, just so you know if it is running or if it has locked up, because I write crap code.
     oled.println(sp);
-    i++;
     
 
+    // makes the spinner spin
+    i++;    
+
     if(i > 7){
+
       i = 0;
     }
-
-		//*Serial.print("CO2 equivalent: "); Serial.print(iaq.getCO2()); Serial.println("ppm");
-		//Serial.print("TVOC equivalent: "); Serial.print(iaq.getTVOC()); Serial.println("ppb");
-		//Serial.print("Sensor Resistance: "); Serial.print(iaq.getResistance()); Serial.println("Ohm");
 	}
   
 
-	else if (iaq.isError())
+	else if (iaq.isError()){
+
 		Serial.println("sensor is in error state");
+
+  }
 
   oled.display();
 }
